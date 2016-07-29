64cfb3b79cbc3e525cab81d75f75c26b2185946e
John Dispennette <jtdartworks@gmail.com>
2016-07-29 07:18:43 -0400

    Working on more server code and screenshots

--- server.js
+++ server.js
@@ -30,6 +30,63 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 app.use(express.static('public'));
 //-----------------------------------------------------------------
+app.get('/scrape', function(req, res){
+    
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+
+            var title, release, rating;
+            var json = { title : "", release : "", rating : ""};
+
+            $('.header').filter(function(){
+                var data = $(this);
+                title = data.children().first().text();
+            
+                release = data.children().last().children().text();
+
+                json.title = title;
+                json.release = release;
+            })
+
+request('http://www.nytimes.com/', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    var parsedResults = [];
+//     $('span.comhead').each(function(i, element){
+//       // Select the previous element
+//       var a = $(this).prev();
+//       // Get the rank by parsing the element two levels above the "a" element
+//       var rank = a.parent().parent().text();
+//       // Parse the link title
+//       var title = a.text();
+//       // Parse the href attribute from the "a" element
+//       var url = a.attr('href');
+//       // Get the subtext children from the next row in the HTML table.
+//       var subtext = a.parent().parent().next().children('.subtext').children();
+//       // Extract the relevant data from the children
+//       var points = $(subtext).eq(0).text();
+//       var username = $(subtext).eq(1).text();
+//       var comments = $(subtext).eq(2).text();
+//       // Our parsed meta data object
+//       var metadata = {
+//         rank: parseInt(rank),
+//         title: title,
+//         url: url,
+//         points: parseInt(points),
+//         username: username,
+//         comments: parseInt(comments)
+//       };
+//       // Push meta-data into parsedResults array
+//       parsedResults.push(metadata);
+//     });
+//     // Log our finished parse results in the terminal
+//     console.log(parsedResults);
+//   }
+// });
+
 
 //Routes-----------------------------------------------------------
 require('./routes/html-routes.js')(app);
